747d201d7e6b268ea4657e0ef27be491c754b73c
EV32114 <63321707+EV32114@users.noreply.github.com>
2020-06-18 16:18:58 +0300

    open image

--- GIF Maker Solution/main/prog.c
+++ GIF Maker Solution/main/prog.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <opencv2/core/core_c.h>
+#include <opencv2/highgui/highgui_c.h>
 
-int main(void)
+int main()
 {
-	printf("hello world\n");
-	getchar();
+
+	cvNamedWindow("Display window", CV_WINDOW_AUTOSIZE); //create a window
+	IplImage* image = cvLoadImage("C:\\Users\\משתמש\\Pictures\\day.png", 1); //create an image. ***change to your image path***
+	if (!image)//The image is empty. 
+	{
+		printf("could not open image\n");
+	}
+	else
+	{
+		cvShowImage("Display window", image);
+		cvWaitKey(0); system("pause");
+		cvReleaseImage(&image);
+	}
 	return 0;
 }
+
